3e2ebb9aecea85d74f2bd767eb13b0f69af850b7
Ashish Jayamohan <ashishjayamohan@Ashishs-MacBook-Pro.local>
2022-11-09 15:41:31 -0800

    updates

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,35 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.List;
+import java.util.ArrayList;
 
 public class TestListExamples {
-  // Write your grading tests here!
+    @Test
+    public void test() {
+        List<String> sample1 = new ArrayList<String>();
+        List<String> sample2 = new ArrayList<String>();
+        sample1.add("a");
+        sample1.add("b");
+        sample1.add("c");
+        sample1.add("d");
+        sample2.add("e");
+        sample2.add("f");
+        sample2.add("g");
+        sample2.add("h");
+        List<String> sample3 = new ArrayList<>();
+        sample3.add("a");
+        sample3.add("b");
+        sample3.add("c");
+        sample3.add("d");
+        sample3.add("e");
+        sample3.add("f");
+        sample3.add("g");
+        sample3.add("h");
+        assertArrayEquals(sample3.toArray(), ListExamples.merge(sample1, sample2).toArray());
+    }
+
+    @Test
+    public void failTest() {
+        assertEquals(5, 2);
+    }
 }
--- out/production/list-examples-grader/grade.sh
+++ out/production/list-examples-grader/grade.sh
@@ -1,15 +1,13 @@
-#set -e
+# set -e
 
 rm -rf student-submission
-rm ListExamples.java
-rm ListExamples.class
 git clone $1 student-submission
 
 if [ -e "student-submission/ListExamples.java" ]
 then
 	echo "ListExamples file found"
 	cp student-submission/ListExamples.java ./
-	javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java 2> error.txt
+	javac -cp ".:./lib/hamcrest-core-1.3.jar:./lib/junit-4.13.2.jar" *.java 2> error.txt
 	if [ -s error.txt ]
 	then
 		echo "Compilation failure"
@@ -18,14 +16,7 @@ then
 		exit
 	else
 		echo "Compilation success"
-		java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > output.txt
-		if [ "$(grep -o E output.txt | wc -l)" != 0 ]
-		then
-		  echo "Errors!"
-		  echo "$(cat output.txt | tail -n2)"
-		else
-		  echo "No errors!"
-		fi
+		java -cp ".:./lib/hamcrest-core-1.3.jar:./lib/junit-4.13.2.jar" TestListExamples
 	fi
 else
 	echo "ListExamples file not found"
